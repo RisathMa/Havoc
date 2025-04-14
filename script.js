a435f65c590c520ca5de4fbe01778112c8e62442
Risath Manvidu <130242297+RisathMa@users.noreply.github.com>
2025-04-14 16:37:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function reveal() {
+    const reveals = document.querySelectorAll('.reveal');
+    
+    reveals.forEach(element => {
+      const windowHeight = window.innerHeight;
+      const elementTop = element.getBoundingClientRect().top;
+      const elementVisible = 150;
+      
+      if (elementTop < windowHeight - elementVisible) {
+        element.classList.add('active');
+      }
+    });
+  }
+  
+  window.addEventListener('scroll', reveal);
+  reveal(); // Initial check
+  
